00919f7d5d9ccedd51ec56a996dfe476892c9a07
Ryan Megarity <ryan.megarity@immerse.io>
2024-04-27 17:43:49 +0100

    Refactor number one - use reduce in score
    This method works but is not the easiest to understand
    next refactor will calculate the score on a frame by frame basis

--- src/bowling.js
+++ src/bowling.js
@@ -1,24 +1,17 @@
 exports.BowlingGame = class {
-  runningScore = 0;
-  numberOfRolls = 0;
-  previousRolls = [];
-  isSpare = false;
+  rolls = [];
   roll(pins) {
-    if (this.isSpare) {
-      this.runningScore += pins * 2;
-      this.isSpare = false;
-    } else {
-      this.runningScore += pins;
-    }
-    if (this.numberOfRolls % 2 === 1) {
-      if (this.previousRolls[this.previousRolls.length - 1] + pins === 10) {
-        this.isSpare = true;
-      }
-    }
-    this.previousRolls.push(pins);
-    this.numberOfRolls++;
+    this.rolls.push(pins);
   }
   score() {
-    return this.runningScore;
+    return this.rolls.reduce((prev, curr, index) => {
+      if (index > 1 && index % 2 === 0) {
+        // spare
+        if (this.rolls[index - 2] + this.rolls[index - 1] === 10) {
+          return prev + curr * 2;
+        }
+      }
+      return prev + curr;
+    }, 0);
   }
 };
