3abe370703df1a627e75cb26b70ace73e0bbb54c
Ryan Megarity <ryan.megarity@immerse.io>
2024-04-27 17:47:49 +0100

    Refactor two
    The code is much more contextually clear by breaking the calculations into 'frames'

--- src/bowling.js
+++ src/bowling.js
@@ -1,17 +1,25 @@
 exports.BowlingGame = class {
   rolls = [];
+
   roll(pins) {
     this.rolls.push(pins);
   }
+
   score() {
-    return this.rolls.reduce((prev, curr, index) => {
-      if (index > 1 && index % 2 === 0) {
-        // spare
-        if (this.rolls[index - 2] + this.rolls[index - 1] === 10) {
-          return prev + curr * 2;
-        }
+    let score = 0;
+    let frameIndex = 0;
+    for (let frame = 0; frame < 10; frame++) {
+      // spare
+      if (this.rolls[frameIndex] + this.rolls[frameIndex + 1] === 10) {
+        score +=
+          this.rolls[frameIndex] +
+          this.rolls[frameIndex + 1] +
+          this.rolls[frameIndex + 2];
+      } else {
+        score += this.rolls[frameIndex] + this.rolls[frameIndex + 1];
       }
-      return prev + curr;
-    }, 0);
+      frameIndex += 2;
+    }
+    return score;
   }
 };
